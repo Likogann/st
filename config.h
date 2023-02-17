715c7968d4d2b069b37b84e6f75d647c4829e829
Likogan <likogan@protonmail.com>
2023-02-16 21:46:47 -0700

    Add scroling patch

--- config.h
+++ config.h
@@ -162,6 +162,10 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+        { ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
+        { ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
+        { 0,            Button4,        kscrollup,      {.i = 1} },
+        { 0,            Button5,        kscrolldown,    {.i = 1} },
 };
 
 /*
