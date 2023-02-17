f56a3a274ca17a8b25e941885bd73576dbd414a6
Likogan <likogan@protonmail.com>
2023-02-16 21:27:15 -0700

    Remove alpha patch

--- apt-install-packages.sh
+++ apt-install-packages.sh
@@ -1,1 +1,1 @@
-sudo apt install xcompmgr build-essential libx11-dev libxinerama-dev sharutils suckless-tools libxft-dev stterm
+sudo apt install build-essential libx11-dev libxinerama-dev sharutils suckless-tools libxft-dev stterm
