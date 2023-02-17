2f344b5a0a9f385b448b3af4c154afe98c9607ed
Likogan <likogan@protonmail.com>
2023-02-16 21:17:56 -0700

    Personal changes

---
+++ apt-install-packages.sh
@@ -0,0 +1,1 @@
+sudo apt install xcompmgr build-essential libx11-dev libxinerama-dev sharutils suckless-tools libxft-dev stterm
---
+++ colors-wal-st.h
@@ -0,0 +1,35 @@
+const char *colorname[] = {
+
+  /* 8 normal colors */
+  [0] = "#0B0F12", /* black   */
+  [1] = "#1B3544", /* red     */
+  [2] = "#2D3C44", /* green   */
+  [3] = "#38454D", /* yellow  */
+  [4] = "#365366", /* blue    */
+  [5] = "#4C5252", /* magenta */
+  [6] = "#5F615E", /* cyan    */
+  [7] = "#adadac", /* white   */
+
+  /* 8 bright colors */
+  [8]  = "#797978",  /* black   */
+  [9]  = "#1B3544",  /* red     */
+  [10] = "#2D3C44", /* green   */
+  [11] = "#38454D", /* yellow  */
+  [12] = "#365366", /* blue    */
+  [13] = "#4C5252", /* magenta */
+  [14] = "#5F615E", /* cyan    */
+  [15] = "#adadac", /* white   */
+
+  /* special colors */
+  [256] = "#0c1013", /* background */
+  [257] = "#adadac", /* foreground */
+  [258] = "#adadac",     /* cursor */
+};
+
+/* Default colors (colorname index)
+ * foreground, background, cursor */
+
+ unsigned int defaultbg = 256;
+ unsigned int defaultfg = 257;
+ unsigned int defaultcs = 258;
+ unsigned int defaultrcs= 258;
