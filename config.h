0c18127d10118004d30cd18c1de955392b03d2aa
likogan <me@likogan.dev>
2023-08-03 21:57:37 -0600

    change indents & spacing

--- config.h
+++ config.h
@@ -99,14 +99,15 @@ float alpha = 0.98;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	[0] = "#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
-	[1] = "#cc241d", /* red     */
-	[2] = "#98971a", /* green   */
-	[3] = "#d79921", /* yellow  */
-	[4] = "#458588", /* blue    */
-	[5] = "#b16286", /* magenta */
-	[6] = "#689d6a", /* cyan    */
-	[7] = "#a89984", /* white   */
+	[0]  = "#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
+	[1]  = "#cc241d", /* red     */
+	[2]  = "#98971a", /* green   */
+	[3]  = "#d79921", /* yellow  */
+	[4]  = "#458588", /* blue    */
+	[5]  = "#b16286", /* magenta */
+	[6]  = "#689d6a", /* cyan    */
+	[7]  = "#a89984", /* white   */
+
 	[8]  = "#928374", /* black   */
 	[9]  = "#fb4934", /* red     */
 	[10] = "#b8bb26", /* green   */
